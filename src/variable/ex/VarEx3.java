07c7cf0395dc4aca547a127ae4711917d0d451c2
kimbro97 <rlagudwog@gmail.com>
2023-12-16 16:20:39 +0900

    feat: 변수

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long num = 10000000000L;
+        boolean type = true;
+        System.out.println(num);
+        System.out.println(type);
+    }
+}

c901aba1f41acd360d1c00d699e1ee922755e887
kimbro97 <rlagudwog@gmail.com>
2023-12-17 15:58:45 +0900

    feat: 스코프와 형변환

---
+++ src/casting/Casting1.java
@@ -0,0 +1,14 @@
+package casting;
+
+public class Casting1 {
+    public static void main(String[] args) {
+        int intValue = 10;
+        long longValue;
+        double doubleValue;
+        longValue = intValue;
+
+        doubleValue = intValue;
+
+        doubleValue = 2000000L;
+    }
+}
---
+++ src/casting/Casting2.java
@@ -0,0 +1,11 @@
+package casting;
+
+public class Casting2 {
+    public static void main(String[] args) {
+        int intValue = 0;
+        long longValue;
+        double doubleValue = 1.5;
+        intValue = (int) doubleValue;
+        System.out.println(intValue);
+    }
+}

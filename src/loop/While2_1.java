c2772341cfe3a92b5e39a6eb8435773b8202c8be
kimbro97 <rlagudwog@gmail.com>
2023-12-17 14:01:22 +0900

    feat: 반목문

---
+++ src/loop/While2_1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class While2_1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        sum = sum + 1;
+        System.out.println("i = " + 1 + " snm = " + sum);
+        sum = sum + 2;
+        System.out.println("i = " + 2 + " snm = " + sum);
+        sum = sum + 3;
+        System.out.println("i = " + 1 + " snm = " + sum);
+    }
+}
